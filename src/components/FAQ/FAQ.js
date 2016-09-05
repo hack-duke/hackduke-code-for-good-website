47814830afe0b06bba6a9865c3addad92e707b1c
Briguy52 <Briguy52@gmail.com>
2016-09-04 22:15:24 -0400

    All 3 columns populated with old FAQ questions

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -28,6 +28,37 @@ export const FAQ = () => (
                    come! We will be providing buses for certain \
                    universities."} />
         </Col>
+        <Col md={4}>
+          <FAQSection title={"Who can participate?"}
+            text={"You can participate as long as you're a student   \
+                  (undergraduate or graduate)! If you're a new   \
+                  grad, we'd love to have you too.  \
+                  \
+                  If you want to be a mentor, see here!"} />
+          <FAQSection title={"Anything I should remember to bring?"}
+            text={"Bring your own ethernet cables (or you can just   \
+                  WiFi) and bring any Nerf gun arsenal you have!"} />
+          <FAQSection title={"What can I make?"}
+            text={"We welcome all software and hardware hacks   \
+                   that align to our tracks for impact. We will also  \
+                   have lab space for hardware hackers."} />
+        </Col>
+        <Col md={4}>
+          <FAQSection title={"Who can I work with?"}
+            text={"Anyone! Get to know your fellow programmers   \
+                  better from your own and other schools!  \
+                  The only request that we have is that you make a   \
+                  new friend by talking to each other, our mentors \
+                  and our experts!"} />
+          <FAQSection title={"When? Where?"}
+            text={"HackDuke is on Saturday, November 13th to   \
+                  Sunday, November 8th. It is hosted on Duke   \
+                  University's West Campus, CIEMAS. More   \
+                  information about travel and parking can be   \
+                  found here."} />
+          <FAQSection title={"How can I become a sponsor?"}
+            text={"Email us at sponsorship@hackduke.org."} />
+        </Col>
       </Row>
 
     </div>
