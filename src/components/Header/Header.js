e29d098f061ee8cd48c16676364948858ed2c226
Brian Lin <brian.lin@duke.edu>
2016-09-26 14:50:22 -0400

    Scroll (#23)
    * Added basic scroll from Nav bar to FAQ
    
    * basic scroll to all sections
    
    * moved all Scroll.Element anchors to HomeView.js
    
    * Fixed lints
    
    * moved scroll logic to function, need to fix arrow jsx bind lint errors
    
    * added jsx no bind rule to eslintrc
    
    * added react-scroll to package.json dependencies
    
    * fixed typo in package.json

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Scroll from 'react-scroll'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import classes from './Header.scss'
 
@@ -6,24 +7,41 @@ import classes from './Header.scss'
 //   <a className={classes.applyButton} eventKey={1} href={'http://my.hackduke.org/login'}>Login</a>
 // </Nav>
 
-export const Header = () => (
-  <div>
-    <Navbar inverse fixedTop>
-      <Navbar.Header>
-        <Navbar.Brand>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <NavItem className={classes.navItem} eventKey={1} href={'#about'}>ABOUT</NavItem>
-          <NavItem className={classes.navItem} eventKey={2} href={'#tracks'}>TRACKS</NavItem>
-          <NavItem className={classes.navItem} eventKey={3} href={'#faq'}>FAQS</NavItem>
-          <NavItem className={classes.navItem} eventKey={4} href={'#sponsors'}>SPONSORS</NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </div>
-)
+class Header extends React.Component {
+
+  scrollToElement (elem) {
+    Scroll.scroller.scrollTo(elem, {
+      duration: 850,
+      delay: 100,
+      smooth: true
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Navbar inverse fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('aboutScrollPoint')}>ABOUT</NavItem>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('tracksScrollPoint')}>TRACKS</NavItem>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('faqScrollPoint')}>FAQS</NavItem>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('sponsorsScrollPoint')}>SPONSORS</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    )
+  }
+}
 
 export default Header
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Scroll from 'react-scroll'
 // import classes from './HomeView.scss'
 import HeroPage from 'components/HeroPage'
 import TracksPage from 'components/TracksPage'
@@ -10,13 +11,13 @@ import Social from 'components/Social'
 export const HomeView = () => (
   <div>
     <HeroPage />
-    <div id={'about'} style={{'transform': 'translateY(-50px)'}}></div>
+    <Scroll.Element name='aboutScrollPoint'></Scroll.Element>
     <About />
-    <div id={'tracks'} style={{'transform': 'translateY(-50px)'}}></div>
+    <Scroll.Element name='tracksScrollPoint'></Scroll.Element>
     <TracksPage />
-    <div id={'faq'} style={{'transform': 'translateY(-50px)'}}> </div>
+    <Scroll.Element name='faqScrollPoint'></Scroll.Element>
     <FAQ />
-    <div id={'sponsors'} style={{'transform': 'translateY(-50px)'}}> </div>
+    <Scroll.Element name='sponsorsScrollPoint'></Scroll.Element>
     <SponsorsPage />
     <Social />
   </div>
