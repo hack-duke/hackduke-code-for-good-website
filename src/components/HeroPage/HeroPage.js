b517d9b0c5bd295e4b1692447696b3351585b0c6
Gideon Pfeffer <gap16@duke.edu>
2017-10-27 15:02:41 -0400

    added dayof

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -6,10 +6,14 @@ export const HeroPage = () => (
     <div className={classes.hero}>
       <div className={classes.mainText}>
         <img src='hackduke-logo.png' alt='Hackduke Logo' />
-        <h6>October 28-29, 2017 <br />Registration Has Closed</h6>
+        <h6> Event this Weekend! </h6>
         <div className={classes.buttonDiv}>
           <a className={classes.applyLink} eventKey={1} href={'http://my.hackduke.org/login'}>
             <button className={classes.loginButton}> Login </button>
+          </a>
+          <a className={classes.applyLink}
+            href={'http://hackduke-dayof.herokuapp.com/'}>
+            <button className={classes.homeButton}> Day Of </button>
           </a>
         </div>
       </div>
