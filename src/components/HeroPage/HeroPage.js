fa4d60a0cde7a4c4122603e5a051f2e5489b20b2
Gideon Pfeffer <gap16@duke.edu>
2018-07-19 20:57:24 -0700

    updates homepage

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -5,20 +5,20 @@ export const HeroPage = () => (
   <div>
     <div className={classes.hero}>
       <div className={classes.mainText}>
-        <img src='hackduke-logo.png' alt='Hackduke Logo' />
-//         <h6> Event this Weekend! </h6>
-        <div className={classes.buttonDiv}>
-          <a className={classes.applyLink} eventKey={1} href={'http://my.hackduke.org/login'}>
-            <button className={classes.loginButton}> Login </button>
-          </a>
-//           <a className={classes.applyLink}
-//             href={'http://hackduke-dayof.herokuapp.com/'}>
-//             <button className={classes.homeButton}> Day Of </button>
-//           </a>
-        </div>
+        // <img src='hackduke-logo.png' alt='Hackduke Logo' />
+        <h6> 2018 Event Oct 13-14 </h6>
       </div>
     </div>
   </div>
+        // <div className={classes.buttonDiv}>
+        //   <a className={classes.applyLink} eventKey={1} href={'http://my.hackduke.org/login'}>
+        //     <button className={classes.loginButton}> Login </button>
+        //   </a>
+        //   <a className={classes.applyLink}
+        //     href={'http://hackduke-dayof.herokuapp.com/'}>
+        //     <button className={classes.homeButton}> Day Of </button>
+        //   </a>
+        // </div>
 )
 
 export default HeroPage
