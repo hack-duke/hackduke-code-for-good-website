763674597efdf9f1df7ee108f99f70c909ce9f10
Chengrui Fu <chengrui.fu@duke.edu>
2018-08-24 02:36:01 -0400

    changes

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -6,16 +6,6 @@ export const AboutSection = (props) => (
 
   <div>
     <Row className={classes.section}>
-      <Col md={4}>
-        <div className={classes.sectionHeader}>
-          {props.title}
-        </div>
-      </Col>
-      <Col md={8}>
-        <div className={classes.sectionText}>
-          {props.text}
-        </div>
-      </Col>
     </Row>
   </div>
 )
